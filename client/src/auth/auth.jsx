114e31721ec0c28c8bafe0790feccbe553a3a8b7
Maiza Aymen <aymenmaiza87@gmail.com>
2024-12-21 20:21:52 +0100

    `Added event listeners for focus and blur on form inputs in auth.jsx`

--- client/src/auth/auth.jsx
+++ client/src/auth/auth.jsx
@@ -3,6 +3,25 @@ import "./auth.css"
 
 
 const Auth=()=>{
+const inputs = document.querySelectorAll(".form__input")
+
+
+function addfocus(){
+    let parent = this.parentNode.parentNode
+    parent.classList.add("focus")
+}
+
+function remfocus(){
+    let parent = this.parentNode.parentNode
+    if(this.value === ""){
+        parent.classList.remove("focus")
+    }
+}
+
+inputs.forEach(input=>{
+    input.addEventListener("focus",addfocus)
+    input.addEventListener("blur",remfocus)
+})
     return(
         <div class="l-form">
         <div class="shape1"></div>
