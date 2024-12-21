4e2670ede4fe14bf4d5b6f99cd2665317fb9ee9f
Maiza Aymen <aymenmaiza87@gmail.com>
2024-12-21 20:27:14 +0100

    `Changed social login links to buttons in auth component`

--- client/src/auth/auth.jsx
+++ client/src/auth/auth.jsx
@@ -83,9 +83,10 @@ const Auth = () => {
           <div className="form__social">
             <span className="form__social-text">Or login with</span>
 
-            <a href="#" className="form__social-icon"><i className='bx bxl-facebook'></i></a>
-            <a href="#" className="form__social-icon"><i className='bx bxl-google'></i></a>
-            <a href="#" className="form__social-icon"><i className='bx bxl-instagram'></i></a>
+            <button type="button" className="form__social-icon"><i className='bx bxl-facebook'></i></button>
+<button type="button" className="form__social-icon"><i className='bx bxl-google'></i></button>
+<button type="button" className="form__social-icon"><i className='bx bxl-instagram'></i></button>
+
 
           </div>
         </form>
