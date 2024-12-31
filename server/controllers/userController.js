6624c5bab6c7bade5fbbe851b00368a13cd24e12
Maiza Aymen <aymenmaiza87@gmail.com>
2025-01-01 00:31:44 +0100

    Added authentication check and user data retrieval in userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,21 @@
 import userModel from "../models/userModel.js";
+import { isAuthenticated } from "./authController.js";
 
 
 export const getUserData=async(req,res)=>{
     try {
         const {userId}=req.body;
         const user=await userModel.findById(userId)
+        if (!user) {
+            return res.json({success:false,message:"user not found "})
+            
+        }
+        res.json({success:true,
+            userData:{
+              name:user.name,
+              isAuthenticated:user.isAuthenticated
+            }})
+
         
     } catch (error) {
       return res.json({success:false,message:error.message})
