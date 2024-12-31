28be02670148612a6b9df0db0a0440e992a4fdbe
Maiza Aymen <aymenmaiza87@gmail.com>
2025-01-01 00:37:06 +0100

    Added newline and export statement to userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,4 +2,5 @@ import express from "express"
 import userAuth from "../middleware/userAuth";
 import { getUserData } from "../controllers/userController";
 const userRouter= express.Router();
-userRouter.get("/data",userAuth,getUserData)
+userRouter.get("/data",userAuth,getUserData);
+export default userRouter;
