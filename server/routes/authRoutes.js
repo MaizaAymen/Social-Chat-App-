c288e581cd6c53c054db5b37ed20b27cab4d3fcf
Maiza Aymen <aymenmaiza87@gmail.com>
2024-12-31 22:55:48 +0100

    `Added new route '/sendResetOtp' to authRoutes.js`

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { isAuthenticated, login, logout, register, sendVerifyOtp, verifyEmail } from '../controllers/authController.js';
+import { isAuthenticated, login, logout, register, sendResetOtp, sendVerifyOtp, verifyEmail } from '../controllers/authController.js';
 import userAuth from '../middleware/userAuth.js';
 
 const authRouter = express.Router(); // Correction du nom du routeur
@@ -9,6 +9,7 @@ authRouter.post('/logout', logout);
 authRouter.post('/sendVerifyOtp',userAuth,sendVerifyOtp);
 authRouter.post('/verifyEmail',userAuth,verifyEmail);
 authRouter.post('/isAuthenticated',userAuth,isAuthenticated);
+authRouter.post('/sendResetOtp',sendResetOtp);
 
 
 export default authRouter; // N'oubliez pas d'exporter le routeur
