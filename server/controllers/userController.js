f8866d1a4e9e6e667db054fbfcd5b867e41a5631
Maiza Aymen <aymenmaiza87@gmail.com>
2025-01-01 00:26:47 +0100

    fix userId in usercontroller.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,7 +4,7 @@ import userModel from "../models/userModel.js";
 export const getUserData=async(req,res)=>{
     try {
         const {userId}=req.body;
-        const user=await userModel.findById()
+        const user=await userModel.findById(userId)
         
     } catch (error) {
       return res.json({success:false,message:error.message})
