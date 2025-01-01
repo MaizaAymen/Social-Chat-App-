809ceae8d0903e3fc622964cdfcccadfc4f91cd3
Maiza Aymen <aymenmaiza87@gmail.com>
2025-01-01 13:25:18 +0100

    Added authentication middleware and comments to userAuth.js

--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -20,3 +20,10 @@ const userAuth=async (req,res, next)=>{
     }
 }
 export default userAuth;
+
+/*4. Authentication of User
+We have to authenticate the user whether it is login before or not. 
+The jwt token that we generated before store as a form of cookies in the browser of user.
+Now if he again go to some other page or again go to that route. 
+That tokens help to identify that this user is logged In. 
+So for that we add a middleware for that authentication of the token in our file name auth.js*/
