1c983e93f9fd88919d3987c0009411a62fcc8a7a
Maiza Aymen <aymenmaiza87@gmail.com>
2025-01-01 13:34:26 +0100

    Added comments to authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -329,3 +329,6 @@ exports.updateUserRole = catchAsyncErrors(async (req, res, next) => {
     message: "Role Updated Successfully"
    })
 });*/
+
+
+/***************************************************"ili ymsou 5atini ana raw mouch rajel :ama ka t7eb tstanfa3 mino 7el chof : "{https://devendrajohari9.medium.com/nodejs-authentication-authorisation-ee04ff744c80} */
