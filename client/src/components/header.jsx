7fa0c4ab4b8ba754aff43ecc34eef612e25f7add
Maiza Aymen <aymenmaiza87@gmail.com>
2025-01-03 21:34:39 +0100

    `Refactor header component styles and add new elements`

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -3,11 +3,14 @@ import './header.css'
 const header = () => {
   return (
     <div className="header-container">
-    <img src="assets/header_img.png" alt="Header Image" className="header-image" />
+    <img src="assets/header_img.png" alt="Header" className="header-image" />
     <h1 className="header-title">
       Hey Developer{' '}
       <img src="assets/hand_wave.png" alt="Wave" className="wave-image" />
     </h1>
+    <h2>Welcome to our app </h2>
+    <p> let's start with a quik product tour and we will have you up and running in no time ! </p>
+    <button> get started </button>
   </div>
   )
 }
