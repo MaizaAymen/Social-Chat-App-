0030509b89162465f1c42267102aef9f4b5667c9
Maiza Aymen <aymenmaiza87@gmail.com>
2025-01-03 20:58:17 +0100

    `Added Navbar and Header components to Home page`

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import Navbar from '../components/navbar'
+import Header from '../components/header'
 const Home = () => {
   return (
     <div>
-      
+      <Navbar/>
+      <Header/>
     </div>
   )
 }
