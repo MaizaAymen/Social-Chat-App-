d8ed8978654f13496233fa5eca023aecbacaea3a
Maiza Aymen <aymenmaiza87@gmail.com>
2025-01-03 19:29:55 +0100

    `Added login page and updated routes in App.js`

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Auth from './auth/auth';
 import Chat from './Chat/Chat';
 import User from './usernavbar/user';
+import login from './pages/login';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
           <Route path="/" element={<User/>} />
           <Route path="/chat" element={<Chat/>} />
           <Route path="/Auth" element={<Auth/>} />
-          <Route path="/" element={</>} />
+          <Route path="/login" element={<login/>} />
           
         </Routes>
       </div>
