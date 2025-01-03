70b879590260607423c6f2b417b885438a306bec
Maiza Aymen <aymenmaiza87@gmail.com>
2025-01-03 21:38:14 +0100

    "Added useNavigate hook and navigate variable to Home component"

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import Navbar from '../components/navbar'
 import Header from '../components/header'
 import './home.css'
+import { useNavigate } from 'react-router-dom'
 const Home = () => {
+    const navigate= useNavigate();
   return (
     <div className="home-container">
       <Navbar/>
