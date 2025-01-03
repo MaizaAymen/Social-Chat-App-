3b909da7c95a23bd528f64054b35a92688218843
Maiza Aymen <aymenmaiza87@gmail.com>
2025-01-03 20:59:12 +0100

    `Update header component to add alt text and class name to hand wave image`

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import './header.css'
 const header = () => {
   return (
     <div>
         <img src="assets\header_img.png" alt="" />
-        <h1>Hey Developer <img src="assets/hand_wave.png" alt="" /></h1>
+        <h1>Hey Developer <img src="assets/hand_wave.png" alt="Wave" className="wave-image" /></h1>
       
     </div>
   )
