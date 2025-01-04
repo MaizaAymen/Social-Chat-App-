9e364e03765dd7ff3d9dbac2c6d474cf3f4b3bf0
Maiza Aymen <aymenmaiza87@gmail.com>
2025-01-04 16:41:18 +0100

    `Added userData state and setuserData hook to AppContextProvider`

--- client/src/context/Appcontext.jsx
+++ client/src/context/Appcontext.jsx
@@ -4,6 +4,7 @@ export const AppContent = React.createContext();
 export const AppContextProvider=(props)=>{
     const backendUrl= import.meta.env.VITE_BACKEND_URL
     const [islogin,setislogin]=useState(false)
+    const [userData,setuserData]=useState(false)
     const value={
   
     }
