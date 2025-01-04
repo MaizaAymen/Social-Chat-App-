a337effadfd2aad374158b9bbf29aa3831ca6cee
Maiza Aymen <aymenmaiza87@gmail.com>
2025-01-04 16:40:30 +0100

    `Added import of useState and islogin state to AppContextProvider`

--- client/src/context/Appcontext.jsx
+++ client/src/context/Appcontext.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
+
 export const AppContent = React.createContext();
 export const AppContextProvider=(props)=>{
     const backendUrl= import.meta.env.VITE_BACKEND_URL
+    const [islogin,setislogin]=useState(false)
     const value={
   
     }
