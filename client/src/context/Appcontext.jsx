dfa65dc23021f6120d5a80762599926785090a68
Maiza Aymen <aymenmaiza87@gmail.com>
2025-01-04 19:01:59 +0100

    `Update AppContext.jsx to use REACT_APP_BACKEND_URL instead of VITE_BACKEND_URL`

--- client/src/context/Appcontext.jsx
+++ client/src/context/Appcontext.jsx
@@ -4,7 +4,7 @@ import React, { useState, createContext } from "react";
 export const AppContent = createContext();
 
 export const AppContextProvider = (props) => {
-    const backendUrl = import.meta.env.VITE_BACKEND_URL; // Assurez-vous que cette variable est définie
+    const backendUrl = import.meta.env.REACT_APP_BACKEND_URL; // Assurez-vous que cette variable est définie
     const [isLogin, setIsLogin] = useState(false); // Correction de la casse pour les conventions de nommage
     const [userData, setUserData] = useState(null); // `null` est plus courant que `false` pour les données utilisateur
 
