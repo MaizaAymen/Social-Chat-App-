0faaedfecdbb73add80e0072d88b662a3a2bb328
Maiza Aymen <aymenmaiza87@gmail.com>
2025-01-04 16:49:21 +0100

    `Added AppContextProvider wrapper around App component in index.js`

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,8 @@ import { AppContextProvider } from './context/Appcontext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppContextProvider><App /></AppContextProvider>
+    
   </React.StrictMode>
 );
 
