8fd81e99b8f8ab0ba4239c47698580296ffc0a6d
Maiza Aymen <aymenmaiza87@gmail.com>
2025-01-04 12:33:09 +0100

    `Added value to AppContextProvider and updated AppContent.Provider`

--- client/src/context/Appcontext.jsx
+++ client/src/context/Appcontext.jsx
@@ -1,7 +1,11 @@
 export const AppContent = React.createContext();
 export const AppContextProvider=(props)=>{
+    const value={
+            
+    }
     return (
-          <AppContent.Provider value={}>
+      
+          <AppContent.Provider value={value}>
             {props.children}
           </AppContent.Provider>
 
