748b4406a5998269a297387ee61c631d2645f434
Maiza Aymen <aymenmaiza87@gmail.com>
2025-01-07 01:29:00 +0100

    `Added toast notification for logout error and made logout function asynchronous`

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -3,18 +3,19 @@ import './navbar.css'
 import { useNavigate } from 'react-router-dom'
 import { AppContent } from '../context/Appcontext';
 import axios from 'axios';
+import { toast } from 'react-toastify';
 const Navbar = () => {
     const navigate = useNavigate();
     const {userData ,backendUrl ,setIsLogin ,setUserData}=useContext(AppContent)
-    const logout=()=>{
+    const logout= async()=>{
       try {
         axios.defaults.withCredentials=true;
-        const {data}= await axios.get('http://localhost:4000/api/auth/logout')
+        const {data}=await axios.get('http://localhost:4000/api/auth/logout')
           data.success && setIsLogin(false)
           data.success && setUserData(false)
           navigate('/Home');
       } catch (error) {
-        
+        toast.error(error.message)
       }
     }
  
@@ -28,7 +29,7 @@ const Navbar = () => {
         <ul class="custom-list">
           {!userData.isAcconuntVerified && <li>verify email</li>}
           
-          <li class="last">logout</li>
+          <li class="last" onClick={logout}>logout</li>
         </ul>
         </div>
       </div>
